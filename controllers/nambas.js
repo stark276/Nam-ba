6caf23a91b3f6ee547d0718d85c268fb55a86360
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-07-22 17:02:47 -0700

    Users must be logged in to submit a nambas

--- controllers/nambas.js
+++ controllers/nambas.js
@@ -4,22 +4,25 @@ const Namba = require('../models/namba');
 module.exports = (app) => {
 
   app.get('/', (req, res) => {
+    var currentUser = req.user;
+
     Namba.find({}).lean()
   .then(nambas => {
-    res.render("namba-index", { nambas });
+    res.render("namba-index", { nambas, currentUser });
   })
   .catch(err => {
     console.log(err.message);
   });
  })
 
- // NEW
+ // NEW FORM
  app.get('/nambas/new/', (req, res) => {
    res.render('nambas-new', {});
  })
 
   // CREATE
   app.post('/nambas/new', (req, res) => {
+    if (req.user) {
     // INSTANTIATE INSTANCE OF POST MODEL
     const namba = new Namba(req.body);
 
@@ -29,6 +32,9 @@ module.exports = (app) => {
       console.log(err)
       return res.redirect(`/`);
     })
+    } else {
+    return res.status(401); // UNAUTHORIZED
+  }
   });
 
   app.get("/nambas/:id", function(req, res) {
--- server.js
+++ server.js
@@ -22,6 +22,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
 app.engine('handlebars', hbs.engine);
+var checkAuth = (req, res, next) => {
+  console.log("Checking authentication");
+  if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+    req.user = null;
+  } else {
+    var token = req.cookies.nToken;
+    var decodedToken = jwt.decode(token, { complete: true }) || {};
+    req.user = decodedToken.payload;
+  }
+
+  next();
+};
+app.use(checkAuth);
+
 app.set('view engine', 'handlebars');
 
 
